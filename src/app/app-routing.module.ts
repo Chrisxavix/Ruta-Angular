de3518c3c48e933a84b00b4fd598c37cd9a9c178
chris <cxchillogalli@engicoders.com>
2021-02-26 17:01:41 -0500

    feat: rutas redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './componentes/about/about.component';
+import { ContactComponent } from './componentes/contact/contact.component';
+import { InfoComponent } from './componentes/info/info.component';
+import { NoPageComponent } from './componentes/no-page/no-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'informacion', component: InfoComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: NoPageComponent },
+  /* o redirigir */
+  /* { path: '**', redirectTo: "informacion"}, */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InfoComponent } from './componentes/info/info.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { ContactComponent } from './componentes/contact/contact.component';
+import { NoPageComponent } from './componentes/no-page/no-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent,
+    AboutComponent,
+    ContactComponent,
+    NoPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
